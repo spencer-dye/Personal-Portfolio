b33aed4f7d1fc92e665444ddc83f6f1cb6c37396
spencer-dye <101621857+spencer-dye@users.noreply.github.com>
2022-06-17 21:27:59 -0600

    Progress

---
+++ src/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".typed-text", {
+    strings: [
+      "^1000 Develop", 
+      "^1000 Design",
+      "^1000 Problem Solve", 
+      "^1000 Build",
+      "^1000 Learn",
+    ],
+    typeSpeed: 65,
+    backSpeed: 50,
+    backDelay: 2000,
+    startDelay: 250,
+    smartBackspace: true, //Default value
+    loop: true,
+    showCursor: false,
+    cursorChar: "|",
+    attr: null,
+  });

a86ef0cf5848b07d70c6666c605948a5b1af5e55
spencer-dye <101621857+spencer-dye@users.noreply.github.com>
2022-06-28 12:24:10 -0600

    commit on main

--- root/js/script.js
+++ root/js/script.js
@@ -1,6 +1,9 @@
   let mobileMenu = document.getElementById("mobileMenu")
   let mobileNav = document.getElementById("mobileNav")
   let nav = document.getElementById('nav')
+  let mLine1 = document.getElementById('mLine1')
+  let mLine2 = document.getElementById('mLine2')
+  let mLine3 = document.getElementById('mLine3')
   let menuStatus = false
 
   mobileMenu.addEventListener('click', menuClose)
@@ -9,10 +12,16 @@
     if(menuStatus === false) {
       mobileNav.classList.remove('menu-hide')
       nav.classList.add('menu-open')
+      mLine1.classList.add('m-line-1-active')
+      mLine2.classList.add('m-line-2-active')
+      mLine3.classList.add('m-line-3-active')
       menuStatus = true
     } else {
       mobileNav.classList.add('menu-hide')
       nav.classList.remove('menu-open')
+      mLine1.classList.remove('m-line-1-active')
+      mLine2.classList.remove('m-line-2-active')
+      mLine3.classList.remove('m-line-3-active')
       menuStatus = false
     }
   }
